5058f52065590ed4c92bb72393397a0befd93de7
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2023-12-28 18:47:08 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -30,7 +30,6 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
         ],
     ]
     return buttons

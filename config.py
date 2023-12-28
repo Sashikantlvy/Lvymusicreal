4dbf80ae922ad599c2738c9832cb03f39c634b84
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2023-12-28 18:35:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/sashikantlvy/lvymusicreal",
+    "https://telegra.ph/file/78be765f35211e764a9d5.mp4",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(

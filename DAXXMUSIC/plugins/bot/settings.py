e3c78fa0094290f145b80c9bc51ec2ba72789ee7
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2023-12-31 18:49:51 +0530

    Update settings.py

--- DAXXMUSIC/plugins/bot/settings.py
+++ DAXXMUSIC/plugins/bot/settings.py
@@ -388,14 +388,3 @@ async def vote_change(client, CallbackQuery, _):
         )
     except MessageNotModified:
         return
-
-
-@app.on_callback_query(filters.regex("gib_source") & ~BANNED_USERS)
-@languageCB
-async def gib_repo(client, CallbackQuery, _):
-    await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://telegra.ph/file/28cd99b77e752366cd7b7.mp4"),
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
-        ),
-    )

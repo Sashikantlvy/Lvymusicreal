2500109207ca0b7a14925cab48209d6f16ac2669
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2024-01-17 17:16:25 +0530

    Update settings.py

--- DAXXMUSIC/plugins/bot/settings.py
+++ DAXXMUSIC/plugins/bot/settings.py
@@ -49,6 +49,16 @@ async def settings_mar(client, message: Message, _):
     )
 
 
+@app.on_callback_query(filters.regex("gib_source") & ~BANNED_USERS)
+@languageCB
+async def gib_repo(client, CallbackQuery, _):
+    await CallbackQuery.edit_message_media(
+        InputMediaVideo("https://graph.org/file/69b3f128014d53bca943a.mp4"),
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="✭ʙᴀᴄᴋ✭", callback_data=f"settingsback_helper")]]
+        ),
+    )
+
 @app.on_callback_query(filters.regex("settings_helper") & ~BANNED_USERS)
 @languageCB
 async def settings_cb(client, CallbackQuery, _):

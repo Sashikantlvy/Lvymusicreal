52a4475bb3ed6b558e3c7dbdd47623d8f4a32212
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2024-01-17 10:39:40 +0530

    Update help.py

--- DAXXMUSIC/plugins/bot/help.py
+++ DAXXMUSIC/plugins/bot/help.py
@@ -38,6 +38,7 @@ async def helper_private(
         language = await get_lang(update.chat.id)
         _ = get_string(language)
         keyboard = help_pannel(_)
+        await update.reply_sticker("CAACAgUAAxkBAAIjVmKPYTFByKZlCo9d8mUv8QVAJEw7AAL9BQACiy14VGoQxOCDfE1KJAQ")
         await update.reply_photo(
             photo=START_IMG_URL,
             caption=_["help_1"].format(SUPPORT_CHAT),
